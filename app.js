e82ecc086c7ef82223c50a45c38c650ff9fe4c8c
Gabriela Guayara <gabriela.guayara@gmail.com>
2024-05-29 11:09:48 -0400

    Built a function to create links into the nav bar

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+menuLinks = [
+    {text: "about", href: "#"},
+    {text: "contact", href: "#"}
+]
+
 const subCtn1 = document.getElementById("sub-ctn1")
 console.log(subCtn1)
 
@@ -13,3 +18,20 @@ const path = `island.mp4`
 source.setAttribute('src', path);
 source.setAttribute('type', 'video/mp4');
 video.append(source);
+
+let menu = document.querySelector("nav .menu")
+console.log(menu)
+
+function getLinks(arr){
+    arr.forEach(aElem =>{
+        let link = document.createElement("a")
+        link.textContent = aElem.text;
+        link.setAttribute("href", aElem.href)
+        menu.append(link)
+    })
+}
+
+getLinks(menuLinks)
+
+let nav = document.querySelector("nav")
+nav.classList.add('flex-between')

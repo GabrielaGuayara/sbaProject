a8a4f8eb81d6448b0933ad047bc556149e935646
Gabriela Guayara <gabriela.guayara@gmail.com>
2024-05-29 10:48:37 -0400

    Created a source tag, set attributtes and added to the html doc

--- app.js
+++ app.js
@@ -7,4 +7,9 @@ console.log(videoCtn)
 let video = document.createElement('video');
 console.log(video)
 videoCtn.append(video)
-
+video.setAttribute('id', 'galapagos-video');
+const source = document.createElement('source');
+const path = `island.mp4`
+source.setAttribute('src', path);
+source.setAttribute('type', 'video/mp4');
+video.append(source);

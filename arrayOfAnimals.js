c88e241e2e1942044ff84db01670bb420d3f15b9
Gabriela Guayara <gabriela.guayara@gmail.com>
2024-05-30 12:25:56 -0400

    Created a file to store an array of animal info

---
+++ arrayOfAnimals.js
@@ -0,0 +1,23 @@
+const cards = [
+    {title: "Mockingbird", text: "One thing that’s very interesting about these tiny birds is that marine iguanas have come to depend on them. Mockingbirds will shout out a very specific cry when they notice predators, like the Galapagos hawk. These hawks hunt both marine iguanas and mockingbirds. ", imgSrc: "img/mockingbird.jpg"},
+
+    {title: "Frigatebird", text: "The magnificent frigatebirds in the Galapagos are thought to be genetically distinct from other frigatebirds, and don’t mate with those from other areas.They typically lay eggs between mid-December and early April. November is a great time to see their mating rituals.", imgSrc: "img/frigatebird.jpg"},
+
+    {title: "Pacific Green Turtle", text: "Pacific green turtles start to lay their eggs, and nesting happens through to March. They’re actually the only species of sea turtle to nest in the Galapagos. These gentle swimmers are commonly seen when snorkeling and boating.They don’t become mature until they’re between 26 and 40 years old, the same as many people.", imgSrc: "img/turtle.jpg"},
+    
+    {title: "Whale Shark", text: "They’re the largest fish on the planet. They can reach up to 46 feet in length and weigh up to 12 tons! Unlike other sharks that only use their tails to swim, whale sharks propel themselves forward by slowly moving their entire body from side to side.", imgSrc: "img/whale-shark.jpg"},
+
+    {title:"The Sea Leon", text: "The sea lion is one of the first animals you’re likely to encounter during your Galapagos Islands vacation as there are over 20,000 of them in the island chain.Sea lions are often spotted lounging on rocks, swimming in the ocean, or socializing on land with other sea lions,", imgSrc: "img/seals.jpg"},
+    
+    {title:"Land Iguanas", text: "Land iguanas can develop to be multiple feet long, weigh as much as 30 pounds, and may live to be 50 years of age. During the day, they can be difficult to spot as they mix in well with the stones and vegetation. Land iguanas are cold-blooded reptiles, so they go through the day heating up in the sun or hanging in the shade to keep cool. ", imgSrc: "img/iguana.jpg"},
+    
+    {title:"Galapagos penguins", text: "They are special because they are the only penguin species that is found north of the equator. These small animals are usually found in the colder areas of the Galapagos, mainly on Fernandina Island and Isabela Island, and tend to live in caves or crevices", imgSrc: "img/penguin.jpg"},
+
+    {title:"Darwin’s Finches", text:"There are 13 different species of Darwin’s finches, the small land bird named after biologist Charles Darwin, found in the Galapagos. While all species of Darwin’s finches are roughly the same size and color, they mostly vary by beak size and diet", imgSrc:"img/tommaso.jpg"},
+    {title:"The blue-footed booby", text:" The blue-footed booby is one of the most popular and recognizable Galapagos animals. Their name comes from their uniquely colored feet, which play an important part in their mating process. Males with more intense hues of blue on their feet are seen as more virile and ultimately attract more females. ", imgSrc:"img/blue-footed.jpg"},
+
+    {title:"The Pink Flaming", text:"The bright pink flamingos of the Galapagos are similar in many ways to the American flamingo, except this species is much smaller and lays tinier eggs.Flamingos mainly feed on algae, crustaceans, and plants that contain high amounts of a carotenoid pigment that affects the intensity of the bird’s pink plumage and skin.", imgSrc:"img/flamingo.jpg"}
+
+];
+
+export {cards};

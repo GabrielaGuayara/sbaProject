fd88745ca07b46cb950f797e606e61af910b1de6
Gabriela Guayara <gabriela.guayara@gmail.com>
2024-05-31 14:50:03 -0400

    Addeed comments and deleted innecesary code

--- app.js
+++ app.js
@@ -1,19 +1,28 @@
+//Import the card array from the arrayOfAnimals file
 import {cards} from "./arrayOfAnimals.js"
 
-//Setting NavBar
+//Personalize website by using window.prompt()
+let nameFromUser = window.prompt('Enter your name');
+document.getElementById('name').innerText = nameFromUser;
+
+
+
+//Setting NavBar by using an array of links
 const menuLinks = [
     {text: "About", href: "#sub-ctn2"},
     {text: "Contact", href: "#sub-ctn3"}
 ]
-
 let navBar = document.querySelector('nav')
-navBar.classList.add('flex-between')
-navBar.style.background = 'var(--nav-ctn)'
+    navBar.classList.add('flex-between')
+    navBar.style.background = 'var(--nav-ctn)'
+
 let navLinks = document.querySelector("nav .menu-links")
 let span = document.getElementById('logoImage')
+    span.appendChild(logoImg)
 let logoImg = document.createElement('img')
-span.appendChild(logoImg)
-logoImg.setAttribute("src",'img/logo.jpg')
+    logoImg.setAttribute("src",'img/logo.jpg')
+
+//Funtion that takes the array of links and asign them content and href attribute
 function getLinks(arr){
     arr.forEach(aElem =>{
         let link = document.createElement("a")
@@ -23,17 +32,20 @@ function getLinks(arr){
     })
 }
 
+//Added a css class to links
 navLinks.classList.add('flex-between')
 
-//First section
-const subCtn1 = document.getElementById("sub-ctn1")
+
+
+//FIRST SECTION
 const videoCtn = document.querySelector("#sub-ctn1 .video-ctn")
+
+//Creates a video and source tag. Then, sets attributes for the source tag
 let video = document.createElement('video');
-
-
 videoCtn.append(video)
 video.setAttribute('id', 'galapagos-video');
 video.setAttribute('width', '100%')
+
 const source = document.createElement('source');
 const path = `img/island.mp4`
 source.setAttribute('src', path);
@@ -43,19 +55,20 @@ video.autoplay = true;
 video.muted = true;
 video.append(source);
 
+//Calls the funtion tue getlInks funtion
 getLinks(menuLinks)
 
 
 
-//Second section
+//SECOND SECTION
 let subCtn2 = document.querySelector("#sub-ctn2 #cardsContainer")
 subCtn2.classList.add("cardsCtn")
 
-
-
+//Funtion that takes the array of animals and built the card for the second section
 function getCards(cards){
     subCtn2.innerHTML = "";
     const maxCards = 3;
+    const useDocFrag = document.createDocumentFragment();
     for(let i = 0; i < maxCards; i++){
         let title = document.createElement('h3')
             title.innerText = cards[i].title;
@@ -67,19 +80,16 @@ function getCards(cards){
         let button = document.createElement("button")
             button.textContent = "Know More"
             image.setAttribute("src", cards[i].imgSrc)
-            card.append(title)
-            card.append(image)
-            // card.append(button)
-            subCtn2.append(card)
+            card.appendChild(title)
+            card.appendChild(image)
             card.append(parag)
-            // button.classList.add("knowMoreBtn")
+             useDocFrag.appendChild(card)
     }
+    subCtn2.append(useDocFrag);
 }
 
-
+//Selects the get more animals buttons from the html and then uses it to get random animals by using an event listener
 const getMoreBtn = document.getElementById("getMore")
-console.log(getMoreBtn)
-
 getMoreBtn.addEventListener('click', (e)=>{
     subCtn2.innerHTML = "";
     const newCards = [];
@@ -94,49 +104,49 @@ getMoreBtn.addEventListener('click', (e)=>{
     getCards(newCards);
 })
 
-
+//Call the getCards function that creates cards for the second section
 getCards(cards)
-// console.log(subCtn2)
-let knowMoreBtns = document.querySelectorAll('.card-ctn button');
 
 
 
-//Third section
+//THIRD SECTION
 const subCtn3 = document.getElementById("sub-ctn3")
-// console.log(subCtn3)
 subCtn3.classList.add('centerForm')
 
 
 //FORM VERIFICATION
+//Caches the submit button into a variable called submitBtn
 let submitBtn = document.querySelector('form button[type="submit"]');
 const errorMessg = document.getElementById('error')
-console.log(errorMessg)
+
+//Using event listener to verify that the user fills all the required fields.
 submitBtn.addEventListener('click', (e)=>{
     e.preventDefault();
     let inpFields = document.querySelectorAll('form input[type="text"]');
-    let isValid = true;
+    console.log(inpFields)
+    let submitted= true;
     let messagesErrors =[];
     
+        //For Loop to iterate the nodelist that contains the name and lastname fields.
         for(let i = 0; i < inpFields.length; i++){
-            if(inpFields[i].value === "" || inpFields === null){
-                isValid = false;
+            if(inpFields[i].value === "" || inpFields[i] === null){
+                submitted = false;
+                messagesErrors.push(`Your ${inpFields[i].name} is required`)
             }
         }
-        if(!isValid){
-            messagesErrors.push("ERROR: Please fill in all fields")
-            errorMessg.innerText = messagesErrors;
-            errorMessg.style.background = 'red'
-
+       
+        if(!submitted){
+            errorMessg.innerText = messagesErrors.join(',');
+            errorMessg.style.background = 'red';
         }else{
-            window.alert("Form submitted sucessfully")
-            errorMessg.display = 'none'
+        window.alert("Form submitted sucessfully")
+        errorMessg.innerHTML = ""
+        errorMessg.style.display= 'none';
+        document.querySelector('form').reset()
+
         }
-})
 
+    })
 
-//Personalize website by using window.prompt()
-let nameFromUser = window.prompt('Enter your name');
-document.getElementById('name').innerText = nameFromUser;
-
-
+//Adding a color to the footer by using customized css
 document.querySelector('footer').style.background = 'var(--nav-ctn)'
